c30970325beac4ace307bf430d6f428957c0ded1
takanori-matsushita <t.matsushita@arcjobz.com>
2022-02-09 01:25:18 +0900

    [fix]test material ui button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 export default function Home() {
   return (
@@ -17,7 +19,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -50,6 +52,10 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <Button variant="contained" color="success" endIcon={<SendIcon />}>
+          material ui test button
+        </Button>
+        <br />
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +64,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
